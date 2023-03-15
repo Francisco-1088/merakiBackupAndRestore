50c1428f207339af3d3d48798b5e232fbe97d5df
Francisco-1088 <32076369+Francisco-1088@users.noreply.github.com>
2023-03-15 17:32:57 -0400

    Update config.py
    Added org filter capabilities

--- config.py
+++ config.py
@@ -2,6 +2,9 @@ API_KEY= ''
 backup_tag = 'merakiBackup'
 restore_tag = 'merakiRestore'
 backup_directory = './backup'
+# Optional, only use one of the following
+org_number_filter = [''] # Optional, add any Org IDs comma separated
+org_name_filter = 'Fran' # Optional, initial org discovery will filter your organizations for org names containing the string
 # Python logging
 logging_level="DEBUG" #DEBUG,INFO,ERROR
 # Meraki SDK Logs
