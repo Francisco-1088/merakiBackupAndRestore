6fbfcec36b1af093dd9327c2b2cd3980c03b53b6
Francisco-1088 <32076369+Francisco-1088@users.noreply.github.com>
2023-03-16 08:56:15 -0400

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ restore_tag = 'merakiRestore'
 backup_directory = './backup'
 # Optional, only use one of the following
 org_number_filter = [''] # Optional, add any Org IDs comma separated
-org_name_filter = 'Fran' # Optional, initial org discovery will filter your organizations for org names containing the string
+org_name_filter = '' # Optional, initial org discovery will filter your organizations for org names containing the string
 # Python logging
 logging_level="DEBUG" #DEBUG,INFO,ERROR
 # Meraki SDK Logs
